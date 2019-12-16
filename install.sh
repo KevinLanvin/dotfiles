eebeeb3df94d320d2014688d51ded781d2a93a27
Kevin Lanvin <info.lanvink@gmail.com>
2019-12-16 12:51:23 +0100

    Move config files and explicit install script

--- install.sh
+++ install.sh
@@ -1,15 +1,36 @@
 #! /bin/sh
 
+try_cmd()
+{
+	MY_CMD=$@
+	eval $MY_CMD 1>/dev/null 2>&1
+	if [ $? = 0 ]; then
+		echo "OK"
+	else
+		echo "Failed"
+	fi
+}
+
 
 # SSH
-# Generate key
-ssh-keygen -t rsa -b 4096 -C info.lanvink@gmail.com -A
-# Add the key to the ssh-agent
-ssh-add ~/.ssh/id_rsa
+if [ ! -f ~/.ssh/id_rsa ]; then
+	echo "Generating ssh key"
+	# Generate key
+	try_cmd ssh-keygen -t rsa -b 4096 -C info.lanvink@gmail.com -A
+	# Add the key to the ssh-agent
+	try_cmd ssh-add ~/.ssh/id_rsa
+fi
 
 # Git
-apt-get -y install git
+echo "Installing git"
+try_cmd sudo apt-get -y install git
 
+# i3
+echo "Installing i3"
+try_cmd sudo apt-get -y install i3
 
 # Copy config files
-cp ./config/xinitrc ~/.xinitrc
+echo "Copying dotfiles"
+try_cmd cp ./config/`ls -a config | sed '/^\.*$/d'` ~/
+
+echo "Setup complete"
